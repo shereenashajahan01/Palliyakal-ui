7aaed12aeea07df56a324fd754db5a3d82441e2f
shereenashajahan01 <80378536+shereenashajahan01@users.noreply.github.com>
2021-08-19 07:45:08 +0400

    updated server.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const path = require('path')
 const app = express()
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/palliyakal-ui'))
+app.use(express.static('./dist/EnginePricingTool-UI'))
 
-app.get('*', (req, res) => {
-  res.sendFile(`./front-end/dist/index.html`) // load the single view file (angular will handle the page changes on the front-end)
-})
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', { root: 'dist/EnginePricingTool-UI/' }),
+)
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080)
