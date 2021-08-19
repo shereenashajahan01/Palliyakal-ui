cbb85898f4750fa1887732ceb04ed6dbb3e2719b
shereenashajahan01 <80378536+shereenashajahan01@users.noreply.github.com>
2021-08-19 07:39:50 +0400

    server.js updated

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const app = express()
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/palliyakal-ui'))
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', { root: 'dist/palliyakal-ui/' }),
-)
-
+app.get('*', (req, res) => {
+  res.sendFile(`./front-end/dist/index.html`) // load the single view file (angular will handle the page changes on the front-end)
+})
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080)
